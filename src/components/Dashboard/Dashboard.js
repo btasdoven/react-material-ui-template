af56406235144c7baf072580f06e7b074811130a
Batuhan Tasdoven <btasdoven@gmail.com>
2018-06-22 12:36:19 -0700

    dashboard changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,6 +10,54 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import TextField from 'material-ui/TextField';
 import FontIcon from 'material-ui/FontIcon';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CardNext from '@material-ui/core/Card';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import CardContentNext from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+import NoteAdd from '@material-ui/icons/NoteAdd';
+import Icon from '@material-ui/core/Icon';
+import { WeightKilogramIcon } from 'mdi-react';
+
+const styles = theme => ({
+  card: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    width: '100%',
+  },
+  content: {
+    textAlign: 'center',
+    width: '100%',
+  },
+  cover: {
+    width: 151,
+    height: 151,
+  },
+  controls: {
+    textAlign: 'center',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  playIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  infoIcon: {
+    fontSize: 44, marginRight: 10, paddingTop: 4
+  },
+});
+
 class Dashboard extends Component {
 
   render() {
@@ -17,36 +65,79 @@ class Dashboard extends Component {
       return (<LoadingIcon />)
     }
 
+    const { classes, theme } = this.props;
+
+    var WrapperForCardAction = (val, label, MyIcon, actionLabel) =>
+    {
+      return (
+        <CardNext className={classes.card}>
+          <div className={classes.details}>
+            <CardContentNext className={classes.content}>
+              <Typography variant="display3">{val}</Typography>
+              <Typography variant="subheading" color="textSecondary">{label}</Typography>
+            </CardContentNext>
+            <Divider/>
+            <div className={classes.controls}>
+              <Button color="secondary" style={{flex:1, borderRadius: 0}}>
+                {MyIcon}
+                {actionLabel}
+              </Button>
+            </div>
+          </div>
+        </CardNext>
+      )
+    }
+
+    var WrapperForCardInfo = (val, label, myIcon) =>
+    {
+      return (
+        <CardNext className={classes.card}>
+          <div className={classes.details}>
+            <CardContentNext className={classes.content}>
+              <Typography variant="display3">{myIcon}{val}</Typography>
+              <Typography variant="subheading" color="textSecondary">{label}</Typography>
+            </CardContentNext>
+          </div>
+        </CardNext>
+      )
+    }
+
+    var clientCount = WrapperForCardAction(32, 'Kayıtlı Danışanım', <PersonAdd className={classes.playIcon} />, 'Yeni Danışan Ekle');
+    var dietListCount = WrapperForCardAction(11, 'Hazır Diyet Listem', <NoteAdd className={classes.playIcon} />, 'Yeni Liste Oluştur');
+    
+    var meetingCount = WrapperForCardInfo(11, 'Bu Hafta Görüştüğüm Danışanlarım', <Icon className={classes.infoIcon}>event</Icon>);
+    var meetingCount = WrapperForCardInfo(41, 'Bu Haftada Danışanlarımın Kaybettiği Kilo', <WeightKilogramIcon className={classes.playIcon} />);
+
     return (
       <div>
         <ProfileAppBar title="Ana Sayfa"/>
-        <Grid fluid style={{marginTop: 12}}>
-          <Row>
-            <Col xs={12} md={4} lg={4}>
-              <Card>
-                <CardHeader
-                  title="Danışanlarına bildirim gönder"
-                  avatar={<FontIcon className="material-icons">notification_important</FontIcon>}
-                />
-                <CardActions>
-                    <TextField
-                      hintText="Mesajını yaz"/>
-                    <FlatButton label="Gönder"/>
-                </CardActions>
-              </Card>
+        <Grid fluid>
+          <Row style={{marginTop: 12}}>
+            <Col xs={12} md={3} lg={3}>
+              {clientCount}
             </Col>
-            <Col xs={12} md={4} lg={4}>
-              <Card>
-                <CardHeader
-                  title="Danışanlarına bildirim gönder"
-                  avatar={<FontIcon className="material-icons">notification_important</FontIcon>}
-                />
-                <CardActions>
-                    <FlatButton label="Yeni Danışan Profili Oluştur"/>
-                </CardActions>
-              </Card>
+            <Col xs={12} md={3} lg={3}>
+              {dietListCount}
             </Col>
-            <Col xs={12} md={4} lg={4}>
+            <Col xs={12} md={3} lg={3}>
+              {clientCount}
+            </Col>
+            <Col xs={12} md={3} lg={3}>
+              {clientCount}
+            </Col>
+          </Row>
+          <Row style={{marginTop: 12}}>
+            <Col xs={12} md={3} lg={3}>
+              {meetingCount}
+            </Col>
+            <Col xs={12} md={3} lg={3}>
+              {meetingCount}
+            </Col>
+            <Col xs={12} md={3} lg={3}>
+              {meetingCount}
+            </Col>
+            <Col xs={12} md={3} lg={3}>
+              {meetingCount}
             </Col>
           </Row>
         </Grid>
@@ -55,7 +146,13 @@ class Dashboard extends Component {
   }
 }
 
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
 export default compose(
   firebaseConnect(), // withFirebase can also be used
-  connect(({ firebase: { auth } }) => ({ auth }))
+  connect(({ firebase: { auth } }) => ({ auth })),
+  withStyles(styles, { withTheme: true })
 )(Dashboard)
